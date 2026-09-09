99ad9351370300decd4f28462defbba6923733dd
bgashyr648-ops <bgashyr648@gmail.com>
2026-09-09 19:47:50 +0500

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -48,7 +48,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, isAdmins, isCreator, 
             teks += `${randomEmoji} @${mem.id.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ LOVE ┃ MD ✪──";
+        teks += "└──✪ 𝐁𝐀𝐑𝐁𝐈𝐄 ┃ MD ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: participants.map(a => a.id) }, { quoted: mek });
 
